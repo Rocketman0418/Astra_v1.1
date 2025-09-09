8bea34b49dd0433d5d4be9b5459b45d160410330
Clay Speakman <clay@healthrocket.life>
2025-09-09 13:41:24 -0400

    Further reduce header padding for minimal height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const Header: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-blue-600 to-purple-700 shadow-lg py-1 px-4">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-blue-600 to-purple-700 shadow-lg py-0.5 px-4">
       <div className="flex items-center justify-center space-x-3">
         {/* Company logo */}
         <img 
           src="/RocketHub Logo Alt 1 Small.png" 
           alt="RocketHub Logo" 
-          className="w-20 h-20 md:w-24 md:h-24 object-contain"
+          className="w-20 h-20 md:w-24 md:h-24 object-contain my-0.5"
         />
         
         {/* Title and rocket emoji */}
