09ba775cbb41f25454c447ffa87fe6425b593703
Clay Speakman <clay@healthrocket.life>
2025-09-09 13:38:33 -0400

    Increase logo size to match title text height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = () => {
         <img 
           src="/RocketHub Logo Alt 1 Small.png" 
           alt="RocketHub Logo" 
-          className="w-10 h-10 md:w-12 md:h-12 object-contain"
+          className="w-6 h-6 md:w-8 md:h-8 object-contain"
         />
         
         {/* Title and rocket emoji */}
